879932a8b579b884d08dc00601c1e4e0276640be
Valery Davydov <volerej@gmail.com>
2019-10-28 22:07:04 +0300

    changes in cash.py

--- Lesson 6/tasks/cash.py
+++ Lesson 6/tasks/cash.py
@@ -1,23 +1,25 @@
+from time import time, strftime, gmtime, sleep
+
+
 global_dict = dict()
 global_time = dict()
 
+
 def multi(val1, val2):
-	'''первое что пришло в голову'''
+	'''Multiplication'''
 	return val1 * val2
 
 
 def get_time():
 	'''Получение текущего времени и даты'''
-	from time import time, strftime, gmtime
-	
 	time_list = strftime('%y %m %d %H %M %S', gmtime(time()))
 	time_list = int(time_list.replace(' ', ''))
 	
 	return time_list
 	
 	
-def find_oldest_date():
-	'''Поиск 'старейшей' даты'''
+def find_oldest_args():
+	'''Поиск 'старейших' данных'''
 	min_time = 0
 	arg = None
 	for args, time in global_time.items():
@@ -27,27 +29,31 @@ def find_oldest_date():
 		elif time < min_time:
 			min_time = time
 			arg = args
-			
-	print(f'These data will be removed : ({arg}: {min_time})') #для проверки
+	#чтобы посмотерть как код работает
+	print(f'These data will be removed : ({arg}: {min_time})') 
+	
 	return arg
 	
 	
 def decor(func, max_cache):
-	
 	def wrapper(val1, val2):
-		flag = list(filter(lambda args: args == (val1, val2), global_dict))
+		flag = True if (val1, val2) in global_dict else False
+		#flag = list(filter(lambda args: args == (val1, val2), global_dict))
 		
-		if len(global_dict) < max_cache and not flag: #если нет в кэше и кэш не переполнен
+		#если нет в кэше и кэш не переполнен
+		if len(global_dict) < max_cache and not flag: 
 			print('Updating cache...')
 			result = func(val1, val2)
 			global_dict[val1, val2] = result
 			global_time[val1, val2] = get_time()
-		elif flag: #если есть в кэше
+		#если есть в кэше
+		elif flag: 
 			print('Getting from cache...')
-			result = global_dict[flag[-1]]
-		elif len(global_dict) >= max_cache: #кэш переполнен
+			result = global_dict[val1, val2]
+		#кэш переполнен
+		elif len(global_dict) >= max_cache: 
 			print('Clearing cache...')
-			oldest_args= find_oldest_date()
+			oldest_args= find_oldest_args()
 			del global_dict[oldest_args]
 			del global_time[oldest_args]
 			
@@ -58,7 +64,6 @@ def decor(func, max_cache):
 			
 		return result
 	return wrapper
-
 
 
 multi = decor(multi, 3)
@@ -73,17 +78,8 @@ if __name__ == '__main__':
 	multi(2, 3)
 	multi(2, 3)
 	
+	sleep(1)
+	multi(5,5)
+	
 	print(f'\nCurrent global_dict:\n{global_dict}')
 	print(f'Current global_time:\n{global_time}')
-
-
-
-
-
-
-
-
-
-
-
-

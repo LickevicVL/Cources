7afe299bceb7328c846c924fec74234cdd0e4a97
LickevicVL <19941510metalhead@gmail.com>
2019-11-05 23:26:13 +0300

    Add lesson 12

---
+++ Lesson 12/tasks/tasks.py
@@ -0,0 +1,8 @@
+# 1*. Задача об обедующих философах
+# Реализовать решение проблемы об обедубующих философах
+#
+
+import webbrowser
+
+URL = 'https://ru.wikipedia.org/wiki/Задача_об_обедающих_философах'
+webbrowser.open_new_tab(URL)
